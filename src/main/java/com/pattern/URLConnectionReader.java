321845499b06ef740be30c433981286452ee4f1f
quyixiao <2621048238@qq.com>
2019-07-16 23:36:08 +0800

    提交修改

--- src/main/java/com/pattern/URLConnectionReader.java
+++ src/main/java/com/pattern/URLConnectionReader.java
@@ -12,7 +12,6 @@ public class URLConnectionReader {
     // 2.调用URL对象openConnection()方法，得到一个http://www.baidu.com的远程连接对象，这个对象的类型就是URLConnection
     // 3.客户端调用URLConnection对象getInputStream()方法读入远程URL的数据
     // 这个系统在运行时会打印http://www.baidu.com主页全部HTML源代码
-
     public static void main(String[] args)  throws Exception{
         URL yahoo = new URL("http://www.baidu.com");
         URLConnection yc = yahoo.openConnection();
